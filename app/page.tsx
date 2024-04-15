a09dfe1a529a6b96299e552a2d82a4ebada864c5
AustinPardosi <gabrielpardosi26@gmail.com>
2024-04-16 00:12:53 +0700

    feat: add searching algorithm

--- app/page.tsx
+++ app/page.tsx
@@ -2,32 +2,74 @@
 
 import Layout from "@/components/layout";
 import MoviesCarousel from "@/components/movieCarousel";
-import Navbar from "@/components/navbar";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearchChange = (event: any) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSearchSubmit = () => {
+    setSearchQuery(searchTerm);
+  };
+
+  const renderContent = () => {
+    if (searchQuery) {
+      return (
+        <>
+          <div className="flex w-full">
+            <MoviesCarousel page={1} search={searchQuery} />
+          </div>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <p className="text-3xl text-[#FD7014] font-extrabold ">
+            Featured today
+          </p>
+          <div className="flex w-ful">
+            <MoviesCarousel page={1} search={"avengers"} />
+          </div>
+          <p className="text-3xl text-[#FD7014] font-extrabold ">
+            Top 10 on IMDb this week
+          </p>
+          <div className="flex w-ful">
+            <MoviesCarousel page={1} search={"star"} />
+          </div>
+          <p className="text-3xl text-[#FD7014] font-extrabold ">
+            What to watch
+          </p>
+          <div className="flex w-ful">
+            <MoviesCarousel page={1} search={"harry"} />
+          </div>
+        </>
+      );
+    }
+  };
+
   return (
     <Layout>
       <div className="w-full ">
-        <p className="pt-4 text-3xl text-[#FD7014] font-extrabold ">
-          Featured today
-        </p>
-        <div className="flex w-ful">
-          <MoviesCarousel page={1} search={"avengers"} />
+        <div className="flex py-4 w-full max-w-sm items-center space-x-2">
+          <Input
+            className="w-96"
+            type="text"
+            value={searchTerm}
+            onChange={handleSearchChange}
+            placeholder="Search title..."
+          />
+          <Button type="submit" onClick={handleSearchSubmit}>
+            Search
+          </Button>
         </div>
-      </div>
-      <div className="w-full ">
-        <p className="text-3xl text-[#FD7014] font-extrabold ">
-          Top 10 on IMDb this week
-        </p>
-        <div className="flex w-ful">
-          <MoviesCarousel page={1} search={"star"} />
-        </div>
-      </div>
-      <div className="w-full ">
-        <p className="text-3xl text-[#FD7014] font-extrabold ">What to watch</p>
-        <div className="flex w-ful">
-          <MoviesCarousel page={1} search={"harry"} />
-        </div>
+        {renderContent()}
       </div>
     </Layout>
   );
